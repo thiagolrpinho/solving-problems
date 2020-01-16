6787b01fb42c0c1c025de39eff7ad9c46cb135fb
Thiago Luis <thiagoluisrpinho@gmail.com>
2020-01-16 20:54:05 -0300

    :recycle: Green: Faster than 87.54% of the community

--- leetcode/387_first_unique_character_in_a_string.py
+++ leetcode/387_first_unique_character_in_a_string.py
@@ -12,10 +12,12 @@ Note: You may assume the string contain only lowercase letters.
 '''
 
 import pytest
+from collections import Counter
 
 @pytest.mark.parametrize('input_and_output', [
     ("leetcode", 0),
-    ("loveleetcode", 2)])
+    ("loveleetcode", 2),
+    ("", -1)])
 def test_max_profit(input_and_output):
     input_string = input_and_output[0]
     expected_output = input_and_output[1]
@@ -24,4 +26,10 @@ def test_max_profit(input_and_output):
 
 
 def firstUniqChar(s: str) -> int:
-    return False
+    if not s:
+        return -1
+    letters_counted = Counter(s)
+    for i in range(len(s)):
+        if letters_counted[s[i]] == 1:
+            return i
+    return -1
