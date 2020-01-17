0656274c27673063b694853672e496dc4dac6f30
Thiago Luis <thiagoluisrpinho@gmail.com>
2020-01-17 14:09:06 -0300

    :recycle: Red: Not working on leetcode

--- leetcode/21_merge_two_sorted_lists.py
+++ leetcode/21_merge_two_sorted_lists.py
@@ -8,7 +8,7 @@ Output: 1->1->2->3->4->4
 '''
 
 import pytest
-
+import heapq
 
 # Definition for singly-linked list.
 class ListNode:
@@ -35,4 +35,39 @@ def test_title_to_number(input_and_output):
 
 
 def mergeTwoLists(l1: ListNode, l2: ListNode) -> ListNode:
-    return ListNode(0, None)
+    list_of_sorted_lists = [[], []]
+    last_node = l1
+    while last_node:
+        list_of_sorted_lists[0].append(last_node.val)
+        last_node = last_node.next
+    
+    last_node = l2
+    while last_node:
+        list_of_sorted_lists[1].append(last_node.val)
+        last_node = last_node.next
+
+    heap = []
+    final_sorted_list = []
+    heap = [
+        (sublist[0], list_index, 0) 
+        for list_index, sublist in enumerate(list_of_sorted_lists) if sublist]
+    heapq.heapify(heap)
+    # Here we have a heap with the first elements of each sublist
+    while heap:
+        # Now we have to pop the smaller element of the heap
+        item, list_index, item_index = heapq.heappop(heap)
+
+        final_sorted_list.append(item)
+        # Then for each element we take we look for other inside that element initial sublist
+        # Till they're over
+        if item_index + 1 < len(list_of_sorted_lists[list_index]):
+            next_tuple = (list_of_sorted_lists[list_index][item_index+1],
+                            list_index, 
+                            item_index+1)
+            heapq.heappush(heap, next_tuple)
+
+    last_node = None
+    for val in final_sorted_list[::-1]:
+        last_node = ListNode(val, last_node)
+
+    return last_node
