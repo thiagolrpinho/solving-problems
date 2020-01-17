e0ab8bff16e76158db83dc5b771087b1670aafc7
Thiago Luis <thiagoluisrpinho@gmail.com>
2020-01-17 15:18:03 -0300

    :recycle: Red: Created dummy function for 371

---
+++ leetcode/371_sum_of_two_integers.py
@@ -0,0 +1,27 @@
+'''
+Calculate the sum of two integers a and b, but you are not allowed to use the operator + and -.
+
+Example 1:
+371_sum_of_two_integers
+Input: a = 1, b = 2
+Output: 3
+Example 2:
+
+Input: a = -2, b = 3
+Output: 1
+'''
+import pytest
+
+@pytest.mark.parametrize('input_and_output', [
+    ([1, 2], 3),
+    ([-2, 3], 1)])
+def test_get_sum(input_and_output):
+    first_input_integer = input_and_output[0][0]
+    second_input_integer = input_and_output[0][1]
+    expected_output = input_and_output[1]
+    predicted_output = getSum(first_input_integer, second_input_integer)
+    assert expected_output == predicted_output
+
+
+def getSum(a: int, b: int) -> int:
+    return False
