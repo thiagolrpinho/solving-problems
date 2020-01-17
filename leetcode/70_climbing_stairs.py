21ef59e34ea09912324884fc7f7fce1b2c51a7e8
Thiago Luis <thiagoluisrpinho@gmail.com>
2020-01-17 17:04:35 -0300

    :recycle: Green: Solved. Faster runtime than 96.61% of the community

--- leetcode/70_climbing_stairs.py
+++ leetcode/70_climbing_stairs.py
@@ -22,11 +22,13 @@ Explanation: There are three ways to climb to the top.
 3. 2 steps + 1 step
 '''
 import pytest
-
+from functools import lru_cache
 @pytest.mark.parametrize('input_and_output', [
     (2, 2),
     (3, 3),
-    (4, 5)])
+    (4, 5),
+    (5, 8),
+    (10, 89)])
 def test_climb_stairs(input_and_output):
     input_list = input_and_output[0]
     expected_output = input_and_output[1]
@@ -34,5 +36,54 @@ def test_climb_stairs(input_and_output):
     assert expected_output == predicted_output
 
 
+@lru_cache(None)
 def climbStairs(n: int) -> int:
-    return False
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 2
+    else:
+        return climbStairs(n-1) + climbStairs(n-2)
+
+
+def partition_climbStairs(n: int) -> int:
+    partitions = [1 for i in range(n)]
+    index = 1
+    solutions = 1
+    last_index = n
+    while index <= n//2:
+        partitions[index-1] = 0
+        partitions[index] = 2
+        solutions += 1
+        combination_index = index + 1
+        while combination_index < last_index:
+            temp = partitions[combination_index]
+            partitions[combination_index] = partitions[combination_index-1]
+            partitions[combination_index-1] = temp
+            solutions += 1
+            combination_index += 1
+        last_index -= 1
+        index += 1
+    
+    return solutions
+
+
+'''
+    partitions = [0 for i in range(n + 1)]
+    solutions = []
+    index = 1
+    partitions[1] = n
+    while index != 0:
+        x = partitions[index - 1] + 1
+        y = partitions[index] - 1
+        print(x, y)
+        index -= 1
+        while x <= y:
+            partitions[index] = x
+            print(partitions)
+            y -= x
+            index += 1
+        partitions[index] = x + y
+        solutions.append(partitions[:index + 1])
+    return len(solutions)
+'''
