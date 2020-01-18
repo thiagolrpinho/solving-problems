d06f3e78c950f345b01acd705b6b76016ac41752
Thiago Luis <thiagoluisrpinho@gmail.com>
2020-01-18 13:42:29 -0300

    :recycle: Red: Created dummy function for problem 454

--- leetcode/15_3_sum.py
+++ leetcode/15_3_sum.py
@@ -17,7 +17,7 @@ A solution set is:
 '''
 import pytest
 from typing import List # Need to import this so we can use List[int] in args
-from math import log10
+
 @pytest.mark.parametrize('input_and_output', [
     ([-1, 0, 1, 2, -1, -4], [[-1, -1, 2], [-1, 0, 1]]),
     ([0, 0, 0, 0], [[0, 0, 0]])
@@ -71,35 +71,3 @@ def threeSum(numbers: List[int]) -> List[List[int]]:
                             numbers[top_index]))
                         bottom_index += 1
     return list(map(lambda x: list(x), triplets_sum))
-
-def threeSumClosest(numbers: List[int], target: int) -> int:
-    closest_distance = float('inf')
-    closest_number = None
-    numbers.sort()
-    numbers_size = len(numbers)
-    for anchor_index, anchor_number in enumerate(numbers[0:-2]):
-        if anchor_index > 0 and numbers[anchor_index-1] == anchor_number: continue
-        bottom_index, top_index = anchor_index+1, numbers_size-1
-
-        summation = anchor_number + numbers[bottom_index] + numbers[bottom_index+1]
-        if summation > target and abs(summation - target) < closest_distance:
-            closest_distance = abs(summation - target)
-            closest_number = summation
-        else:
-            summation = anchor_number + numbers[top_index] + numbers[top_index-1]
-            if summation < target and abs(summation - target) < closest_distance:
-                closest_distance = abs(summation - target)
-                closest_number = summation
-            else:
-                while(bottom_index < top_index):
-                    summation = anchor_number + numbers[bottom_index] + numbers[top_index]
-                    if abs(summation - target) < closest_distance:
-                        closest_distance = abs(summation - target)
-                        closest_number = summation
-                    if summation > target:
-                        top_index -= 1
-                    elif summation < target:
-                        bottom_index += 1
-                    else: 
-                        return closest_number
-    return closest_number
