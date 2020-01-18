4c47a734daadbca2dc176ade0066bffde80c1021
Thiago Luis <thiagoluisrpinho@gmail.com>
2020-01-18 12:13:34 -0300

    :recycle: Green: Solved. Runtime faster than 93.27% of the community

--- leetcode/15_3_sum.py
+++ leetcode/15_3_sum.py
@@ -19,7 +19,8 @@ import pytest
 from typing import List # Need to import this so we can use List[int] in args
 from math import log10
 @pytest.mark.parametrize('input_and_output', [
-    (([-1, 0, 1, 2, -1, -4]), [[-1, 0, 1], [-1, -1, 2]]),
+    ([-1, 0, 1, 2, -1, -4], [[-1, -1, 2], [-1, 0, 1]]),
+    ([0, 0, 0, 0], [[0, 0, 0]])
     ])
 def test_three_sum(input_and_output):
     input_list = input_and_output[0]
@@ -27,8 +28,49 @@ def test_three_sum(input_and_output):
     predicted_output = threeSum(input_list)
     assert predicted_output == expected_output
 
-def threeSum(nums: List[int]) -> List[List[int]]:
-    return False
+def threeSum(numbers: List[int]) -> List[List[int]]:
+    target = 0
+    numbers.sort()
+    numbers_size = len(numbers)
+    triplets_sum = set()
+    for anchor_index, anchor_number in enumerate(numbers[0:-2]):
+        if anchor_index > 0 and numbers[anchor_index-1] == anchor_number:
+            ''' If it's the same anchor as before. The triplets would be
+                the same '''
+            continue
+        bottom_index, top_index = anchor_index+1, numbers_size-1
+
+        summation = anchor_number + numbers[bottom_index]\
+            + numbers[bottom_index+1]
+        ''' First we try the smaller possible sum for this anchor '''
+        if summation > target:
+            ''' If it's already above the target, then there's nothing
+                else to search for this anchor'''
+            break
+        else:
+            summation = anchor_number + numbers[top_index] + numbers[top_index-1]
+            ''' Then we try the greater possible sum for this anchor '''
+            if summation < target:
+                ''' If it's smaller than the target, then there's
+                    nothing else to search for this anchor
+                '''
+                continue
+            else:
+                ''' If those conditions are not true
+                    then there's still something to find '''
+                while(bottom_index < top_index):
+                    summation = anchor_number + numbers[bottom_index]\
+                        + numbers[top_index]
+                    if summation > target:
+                        top_index -= 1
+                    elif summation < target:
+                        bottom_index += 1
+                    else:
+                        triplets_sum.add((
+                            anchor_number, numbers[bottom_index],
+                            numbers[top_index]))
+                        bottom_index += 1
+    return list(map(lambda x: list(x), triplets_sum))
 
 def threeSumClosest(numbers: List[int], target: int) -> int:
     closest_distance = float('inf')
