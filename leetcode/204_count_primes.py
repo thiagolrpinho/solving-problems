448ca25adf8d94395d5b71308694bfd9207f3839
Thiago Luis <thiagoluisrpinho@gmail.com>
2020-01-18 10:42:22 -0300

    :recycle: Green: Solved. Runtime faster than 91.14% of the community

--- leetcode/204_count_primes.py
+++ leetcode/204_count_primes.py
@@ -10,11 +10,12 @@ Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
 import pytest
 
 @pytest.mark.parametrize('input_and_output', [
-    (10, 4),
+    (10, 3),
     (5, 2),
     (3, 1),
     (6, 3),
-    (8, 4)])
+    (8, 4),
+    (400, 78)])
 def test_count_primes(input_and_output):
     input_natural = input_and_output[0]
     expected_output = input_and_output[1]
@@ -23,4 +24,11 @@ def test_count_primes(input_and_output):
 
 
 def countPrimes(n: int) -> int:
-    return False
+    if n < 2:
+        return 0
+    is_prime_list = [1] * n
+    is_prime_list[0] = is_prime_list[1] = 0
+    for i in range(2, int(n**0.5)+1):
+        if is_prime_list[i] == 1:
+            is_prime_list[i * i:n:i] = [0] * len(is_prime_list[i*i:n:i])
+    return sum(is_prime_list)
