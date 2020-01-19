86dde15137095dfba2e9d3dd46b8538f9707bca0
Thiago Luis <thiagoluisrpinho@gmail.com>
2020-01-19 12:55:18 -0300

    :recycle: Red: Failing for new test case

--- leetcode/198_house_robber.py
+++ leetcode/198_house_robber.py
@@ -25,10 +25,12 @@ Total amount you can rob = 2 + 9 + 1 = 12.
 '''
 import pytest 
 from typing import List
+import heapq
 @pytest.mark.parametrize('input_and_output', [
     ([1, 2, 3, 1], 4),
     ([2, 7, 9, 3, 1], 12),
-    ([2, 1, 1, 2], 4)])
+    ([2, 1, 1, 2], 4),
+    ([1, 7, 9, 4], 11)])
 def test_rob(input_and_output):
     input_integer = input_and_output[0]
     expected_output = input_and_output[1]
@@ -36,4 +38,25 @@ def test_rob(input_and_output):
     assert predicted_output == expected_output
 
 def rob(nums: List[int]) -> int:
-    return max(map(sum, [nums[0:len(nums):2], nums[1:len(nums):2]]))
+    ''' First we calculate the profit
+        of each index as it's value
+        subtracted by the value of
+        not choosing it's neigbours'''
+    heap_profits = [(a[1]+a[2]-a[0], i) for i, a in enumerate(zip(
+        nums, nums[1:]+[0], [0]+nums[:-1]))]
+    ''' As we'll be working with a min-heap, we multiplied
+        the profit by minus 1 so the better profit is choosen first
+    '''
+    heapq.heapify(heap_profits)
+    final_value = 0
+    choosen_index = set()
+    while heap_profits:
+        ''' Now we have to pop the smaller element of the heap '''
+        profit, index = heapq.heappop(heap_profits)
+        if index+1 in choosen_index or index-1 in choosen_index:
+            continue
+        ''' If it's not an option already blocked by other choices,
+            we then add then as one choice '''
+        choosen_index.add(index)
+        final_value += nums[index]
+    return final_value
