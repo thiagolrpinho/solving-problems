7e888141625286409154d10eef174df3256d4661
Thiago Luis <thiagoluisrpinho@gmail.com>
2020-01-19 19:06:39 -0300

    :recycle: Red: Created dummy function for problem 28

---
+++ leetcode/28_implement_str_str.py
@@ -0,0 +1,38 @@
+'''
+mplement strStr().
+
+Return the index of the first occurrence of needle in haystack, or -1 if
+needle is not part of haystack.
+
+Example 1:
+
+Input: haystack = "hello", needle = "ll"
+Output: 2
+Example 2:
+
+Input: haystack = "aaaaa", needle = "bba"
+Output: -1
+Clarification:
+
+What should we return when needle is an empty string? This is a great question
+to ask during an interview.
+
+For the purpose of this problem, we will return 0 when needle is an empty
+string. This is consistent to C's strstr() and Java's indexOf().
+'''
+
+import pytest 
+
+@pytest.mark.parametrize('input_and_output', [
+    (["hello", "ll"], 2),
+    (["aaaaa", "bba"], -1)
+    ])
+def test_str_str(input_and_output):
+    s_input_string = input_and_output[0][0]
+    t_input_string = input_and_output[0][1]
+    expected_output = input_and_output[1]
+    predicted_output = strStr(s_input_string, t_input_string)
+    assert expected_output == predicted_output
+
+def strStr(haystack: str, needle: str) -> int:
+    return False
