af61070b5748771a8ae0501c2cd4b0d4f5333709
Thiago Luis <thiagoluisrpinho@gmail.com>
2020-01-21 16:08:03 -0300

    :recycle: Red: Created dummy function for problem 350

---
+++ leetcode/350_intersection_of_two_arrays_ii.py
@@ -0,0 +1,44 @@
+'''Given two arrays, write a function to compute their intersection.
+
+Example 1:
+
+Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2,2]
+Example 2:
+
+Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+Output: [4,9]
+Note:
+
+Each element in the result should appear as many times as it shows in both
+arrays.
+The result can be in any order.
+Follow up:
+
+What if the given array is already sorted? How would you optimize your
+algorithm?
+What if nums1's size is small compared to nums2's size? Which algorithm is
+better?
+What if elements of nums2 are stored on disk, and the memory is limited such
+that you cannot load all elements into the memory at once?
+'''
+import pytest
+from typing import List
+
+@pytest.mark.parametrize('input_and_output', [
+    (([1,2,2,1], [2,2]), [2,2]),
+    (([4,9,5], [9,4,9,8,4]), [4,9]),
+    ])
+def test_intersect(input_and_output):
+    first_input_integer = input_and_output[0][0]
+    second_input_integer = input_and_output[0][1]
+    expected_output = input_and_output[1]
+    predicted_output = intersect(first_input_integer, second_input_integer)
+    assert expected_output == predicted_output
+
+
+def intersect(nums1: List[int], nums2: List[int]) -> List[int]:
+    return False
+
+
+
